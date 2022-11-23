a88045867de03160adbfab6144247a9a9d7795a7
JackPadalino <pada0867@gmail.com>
2022-11-22 22:25:30 -0500

    added message when user joins chat

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.emit('user joined');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
